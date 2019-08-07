e27b9341a622fd50691faa4f5b19c1cd401f7113
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-07 23:32:41 +0100

    improve main page

--- client/src/Pages/Main/index.tsx
+++ client/src/Pages/Main/index.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import { Heading } from 'rimble-ui';
 import Navbar from '../../Components/Navbar';
 import amazonas from './amazonas.webp';
+import unicefLogo from './unicef-logo.png';
 
 
 const Background = styled.div`
@@ -24,6 +26,9 @@ const Background = styled.div`
     z-index: -1;
     opacity: 0.5;
 `;
+const Content = styled.div`
+    text-align: center;
+`;
 
 interface IMainState {
     //
@@ -35,7 +40,11 @@ class Main extends Component<{}, IMainState> {
             <>
                 <Background />
                 <Navbar />
-
+                <Content>
+                    <img src={unicefLogo} style={{height: '300px'}} />
+                    <Heading.h1>Connect-Ed</Heading.h1>
+                    <Heading.h3 theme={{colors: { grey: '#696969' }}} color='grey'>REDUCING THE DIGITAL DEVIDE</Heading.h3>
+                </Content>
             </>
         );
     }
