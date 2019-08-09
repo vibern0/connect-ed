b4aa9ca958ae9574733d55bc874101aeede07fb8
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-09 12:16:39 +0100

    improve donate page styles

--- client/src/Pages/Donate/index.tsx
+++ client/src/Pages/Donate/index.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Button, Form, Heading, Text } from 'rimble-ui';
+import styled from 'styled-components';
 import Navbar from '../../Components/Navbar';
 
 
+const Content = styled.div`
+    margin: 0px 20%;
+`;
 interface IDonateState {
     donationAmount: string;
 }
@@ -28,17 +33,35 @@ class Donate extends Component<{}, IDonateState> {
         return (
             <>
                 <Navbar />
-                <form onSubmit={this.handleSubmitDonate}>
-                    <input
-                        type="text"
-                        name="donationAmount"
-                        value={donationAmount}
-                        onChange={this.handleDonationAmountChange}
-                    />
-                    <input
-                        type="submit"
-                    />
-                </form>
+                <Content>
+                    <br />
+                    <br />
+                    <br />
+                    <div style={{ textAlign: 'center'}}>
+                        <Heading.h2>Thank you</Heading.h2>
+                        <br />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum dapibus convallis.
+                            Nulla vel laoreet augue. In tincidunt felis sapien, vel molestie felis fringilla nec. Sed
+                            ac malesuada tortor. Etiam tincidunt in arcu eget sollicitudin. Vestibulum tempus blandit
+                            dui, pulvinar facilisis metus pharetra nec. Vivamus sed posuere massa. Aliquam accumsan ex
+                    sit amet ullamcorper ultricies. Nullam ac felis nisl. In id dictum felis.</p>
+                        <br /><br />
+                    </div>
+                    <Form onSubmit={this.handleSubmitDonate} style={{ padding: '0px 30%' }}>
+                        <Form.Field label="Amount to Donate" width={1}>
+                            <Form.Input
+                                type="text"
+                                name="donationAmount"
+                                required={true}
+                                value={donationAmount}
+                                onChange={this.handleDonationAmountChange}
+                                style={{ width: '100%' }}
+                            />
+                        </Form.Field>
+                        <br />
+                        <Button type="submit" width={1}>Donate</Button>
+                    </Form>
+                </Content>
             </>
         );
     }
