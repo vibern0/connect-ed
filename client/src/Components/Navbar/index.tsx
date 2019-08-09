0380d970aee71fd2cadcc56b3fe8cee05f87f3ba
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-09 12:16:27 +0100

    add fake login + logout to navbar

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { Modal, Button, Box, Card, Heading, Text, UPortButton } from 'rimble-ui';
+import { Box, Button, Card, Heading, Modal, Text, UPortButton, Avatar, Flex } from 'rimble-ui';
+import styled from 'styled-components';
 import logo from '../../Assets/logo.webp';
+import avatar from './avatar.png';
 import wifi from './wiki.png';
+
 import './index.css';
 
-
+const NavbarItem = styled.div`
+    margin: 0px 15px;
+    cursor: pointer;
+`;
 interface INavbarState {
+    toogleLogoutModal: boolean;
     toggleSignUpModal: boolean;
 }
 class Navbar extends Component<{}, INavbarState> {
@@ -14,77 +21,149 @@ class Navbar extends Component<{}, INavbarState> {
         super(props);
         this.state = {
             toggleSignUpModal: false,
+            toogleLogoutModal: false,
         };
     }
 
     public toogleSignUpModal = (e: any) => {
+        this.setState((state) => ({ toggleSignUpModal: !state.toggleSignUpModal }));
         e.preventDefault();
-        this.setState((state, props) => ({ toggleSignUpModal: !state.toggleSignUpModal }));
+    }
+
+    public toogleLogoutModal = (e: any) => {
+        this.setState((state) => ({ toogleLogoutModal: !state.toogleLogoutModal }));
+        e.preventDefault();
+    }
+
+    public goMainPage = (e: any) => {
+        (window as any).location.href = '/';
+        console.log('here');
     }
 
     public render() {
         return (
             <>
-                <nav className="navbar" role="navigation" aria-label="main navigation" style={{ height: '160px', padding: '30px 50px 0px 50px' }}>
+                <nav
+                    className="navbar"
+                    role="navigation"
+                    aria-label="main navigation"
+                    style={{ height: '160px', padding: '30px 50px 0px 50px' }}
+                >
                     <div className="navbar-brand">
-                        <img src={logo} height="130px" width="130px" />
+                        <img src={logo} height="130px" width="130px" onClick={this.goMainPage} />
 
-                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
+                        <a
+                            role="button"
+                            className="navbar-burger burger"
+                            aria-label="menu"
+                            aria-expanded="false"
+                            data-target="navbarBasicExample"
+                        >
+                            <span aria-hidden="true" />
+                            <span aria-hidden="true" />
+                            <span aria-hidden="true" />
                         </a>
                     </div>
 
                     <div id="navbarBasicExample" className="navbar-menu">
                         <div className="navbar-end">
-                            <a className="navbar-item" style={{ fontVariantCaps: 'all-petite-caps', fontWeight: 600, fontSize: 'larger' }}>
+                            <span
+                                className="navbar-item"
+                                style={{ fontVariantCaps: 'all-petite-caps', fontWeight: 600, fontSize: 'larger' }}
+                            >
                                 <img src={wifi} style={{ height: '30px', padding: '0px 10px 0px 0px' }} />
                                 Unicef Challenge
-                            </a>
+                            </span>
                             <div className="navbar-item">
-                                <div className="buttons">
-                                    <a className="button is-primary" onClick={this.toogleSignUpModal}>
-                                        <strong>Sign up</strong>
-                                    </a>
-                                </div>
+                                {this.loadCurrentAccount()}
                             </div>
                         </div>
                     </div>
                 </nav >
                 {this.renderSignUpModal()}
+                {this.renderLogoutModal()}
             </>
+        );
+    }
+
+    private loadCurrentAccount = () => {
+        const isLogged = true;
+        const username = 'someusername';
+        const role = 2; // ISP
+        if (isLogged) {
+            return (
+                <Flex>
+                    {role === 2 && <NavbarItem className="navbar-item">ISP</NavbarItem>}
+                    <NavbarItem className="navbar-item">Wecolme, {username}</NavbarItem>
+                    <NavbarItem className="navbar-item">
+                        <Avatar
+                            size="medium"
+                            src={avatar}
+                            onClick={this.toogleLogoutModal}
+                        />
+                    </NavbarItem>
+                </Flex>
+            );
+        }
+        return (
+            <div className="buttons">
+                <a className="button is-primary" onClick={this.toogleSignUpModal}>
+                    <strong>Sign up</strong>
+                </a>
+            </div>
         );
     }
 
     private renderSignUpModal = () => {
         return (
-            <React.Fragment>
-                <Modal isOpen={this.state.toggleSignUpModal}>
-                    <Card width={"420px"} p={0}>
-                        <Button.Text
-                            icononly
-                            icon={"Close"}
-                            color={"moon-gray"}
-                            position={"absolute"}
-                            top={0}
-                            right={0}
-                            mt={3}
-                            mr={3}
-                            onClick={this.toogleSignUpModal}
-                        />
+            <Modal isOpen={this.state.toggleSignUpModal}>
+                <Card width={'420px'} p={0}>
+                    <Button.Text
+                        icononly={true}
+                        icon={'Close'}
+                        color={'moon-gray'}
+                        position={'absolute'}
+                        top={0}
+                        right={0}
+                        mt={3}
+                        mr={3}
+                        onClick={this.toogleSignUpModal}
+                    />
 
-                        <Box p={4} mb={3}>
-                            <Heading.h3>Sign Up</Heading.h3>
-                            <Text>Hello,</Text>
-                            <Text>Welcome to Connect-Ed</Text>
-                            <Text>In order to login you need uPort</Text>
-                            <Text>Please, use the button below</Text>
-                            <UPortButton>Connect with uPort</UPortButton>
-                        </Box>
-                    </Card>
-                </Modal>
-            </React.Fragment>
+                    <Box p={4} mb={3}>
+                        <Heading.h3>Sign Up</Heading.h3>
+                        <Text>Hello,</Text>
+                        <Text>Welcome to Connect-Ed</Text>
+                        <Text>In order to login you need uPort</Text>
+                        <Text>Please, use the button below</Text>
+                        <UPortButton>Connect with uPort</UPortButton>
+                    </Box>
+                </Card>
+            </Modal>
+        );
+    }
+
+    private renderLogoutModal = () => {
+        return (
+            <Modal isOpen={this.state.toogleLogoutModal}>
+                <Card width={'420px'} p={0}>
+                    <Button.Text
+                        icononly={true}
+                        icon={'Close'}
+                        color={'moon-gray'}
+                        position={'absolute'}
+                        top={0}
+                        right={0}
+                        mt={3}
+                        mr={3}
+                        onClick={this.toogleLogoutModal}
+                    />
+
+                    <Box p={4} mb={3}>
+                        <Heading.h3>Logout</Heading.h3>
+                    </Box>
+                </Card>
+            </Modal>
         );
     }
 }
