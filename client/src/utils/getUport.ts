76d67e84fbfbe1f950e54c53922a3ce64e6500df
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-09 21:36:00 +0100

    update getuport

--- client/src/utils/getUport.ts
+++ client/src/utils/getUport.ts
@@ -1,12 +1,11 @@
 import { Connect } from 'uport-connect';
 
 const getUport = () => {
-    const uport = new Connect('ImmunoBlock', {
-        bannerImage: { '/': '/ipfs/QmYD2roj7j455Usn1BjxEsBzHGPj9P3M7tXTkavZMqehYD' },
-        description: 'ImmunoBlock is a project for gitcoin beyound blockchain hackathon.',
+    const uport = new Connect('Connect-Ed', {
+        bannerImage: { '/': '/ipfs/QmdXeiHuqsBgwuDbQi2CkzBRtbgzZpc7jAUXJcYkdZs6nb' },
+        description: 'Welcome to connect-ed.',
         network: 'ropsten',
-        profileImage: { '/': '/ipfs/QmU7ChytERzyUpwrrGEKAs2xvQGQSsNsjM7v4th5LUtdNt' },
-        // network: { id: 5777, rpcUrl: 'http://localhost:8545' },
+        profileImage: { '/': '/ipfs/QmWojBnSsrpBiddLiTudoM4RnbnahmLE3TWm8jyZzKmgRB' },
     });
     return uport;
 };
