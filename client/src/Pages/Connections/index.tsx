35fdbb94dcd9f3c5a5e3735b2162f63d28288133
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-11 00:29:21 +0100

    add pop_amount to all map elements

--- client/src/Pages/Connections/index.tsx
+++ client/src/Pages/Connections/index.tsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
+
+import BlockchainGeneric, { IBasicComponentState } from '../../Common';
+import getUport from '../../utils/getUport';
+
 import Navbar from '../../Components/Navbar';
 import AmazonasMap from './AmazonasMap';
 
-interface IConnectionsState {
-    //
-}
-class Connections extends Component<{}, IConnectionsState> {
+
+class Connections extends Component<{}, IBasicComponentState> {
 
     constructor(props: any) {
         super(props);
+        this.state = {
+            accountsContract: undefined as any,
+            cookies: new Cookies(),
+            uport: getUport(),
+            userAccount: '',
+            web3: undefined as any,
+        };
+    }
+
+    public componentDidMount = async () => {
+        const { cookies } = this.state;
+        const generic = await BlockchainGeneric.onLoad();
+        const accountsContract = await BlockchainGeneric.loadAccountsContract(generic.web3);
+        this.setState({
+            accountsContract,
+            userAccount: generic.userAccount,
+            web3: generic.web3,
+        });
     }
 
     public render() {
+        const { cookies, userAccount, accountsContract, uport } = this.state;
         return (
             <>
+                <Navbar
+                    userAccount={userAccount}
+                    accountsContract={accountsContract}
+                    cookies={cookies}
+                    uport={uport}
+                />
                 <br />
                 <br />
                 <AmazonasMap />
