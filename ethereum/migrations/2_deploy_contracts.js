24d416ca44180112541dc0397d43e74723a95838
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-10 19:10:53 +0100

    deploy all contracts

--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Accounts = artifacts.require('./Accounts.sol');
+const Donations = artifacts.require('./Donations.sol');
+const ISP = artifacts.require('./ISP.sol');
 
-module.exports = (deployer) => {
-    deployer.deploy(Accounts);
+module.exports = async (deployer) => {
+    await deployer.deploy(Accounts);
+    await deployer.deploy(Donations, Accounts.address);
+    await deployer.deploy(ISP, Accounts.address);
 };
